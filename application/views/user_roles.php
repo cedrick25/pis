62a2008b32c4b5f674543c68d65c6876663b28bc
derycedrick <derycedrick@gmail.com>
2023-01-05 18:57:49 -0800

    user_roles.php - integrate: add new user role, roles list modified UI of user role. Addition and modification of modal in add role

--- application/views/user_roles.php
+++ application/views/user_roles.php
@@ -10,67 +10,74 @@
     <!-- modal -->
 
     <!-- Update modal -->
-    <div class="modal fade" id="updateUserModal" tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
+    <div class="modal fade" id="updateDeptModal" tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-md" role="document" style="">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="mediumModalLabel">Update User Type</h5>
+                    <h5 class="modal-title" id="mediumModalLabel">Update User Accounts</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
-                    </button>
+                  </button>
                 </div>
-                    <div class="modal-body col-md-12">
-                        <div class="row form-group col-md-12">
-                            <div class="col col-md-3"><label for="text-input" class=" form-control-label">User Type</label></div>
-                            <div class="col-12 col-md-9"><input type="text" id="text-input" name="text-input" placeholder="USER TYPE" class="form-control"></div>
-                        </div>
-                    </div>                            
+                <div class="modal-body col-md-12">
+                    <div class="row form-group col-md-12">
+                        <div class="col col-md-3"><label for="text-input" class=" form-control-label">Department Name</label></div>
+                        <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="CMRD" class="form-control dep_name_update"></div>
+                        <div class="col col-md-3"><label for="text-input" class=" form-control-label">Description</label></div>
+                        <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="description" class="form-control dep_desc_update"></div>
+                    </div>
+                </div>                            
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-primary">Confirm</button>
+                    <button type="button" class="btn btn-primary btn_confirm_update">Confirm</button>
                 </div>
             </div>
         </div>
     </div>
     <!-- Update modal -->
 
-    <!-- new User roles modal -->
-    <div class="modal fade" id="newUserModal" tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
+    <!-- new User account modal -->
+    <div class="modal fade" id="newDeptModal" tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-md" role="document" style="">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="mediumModalLabel">New User Type</h5>
+                    <h5 class="modal-title" id="mediumModalLabel">New User Role</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
-                    </button>
+                  </button>
                 </div>
-                    <div class="modal-body col-md-12">
-                        <div class="row form-group col-md-12">
-                            <div class="col col-md-3"><label for="text-input" class=" form-control-label">User Type</label></div>
-                            <div class="col-12 col-md-9"><input type="text" id="text-input" name="text-input" placeholder="USER TYPE" class="form-control"></div>
-                        </div>
-                    </div>                            
+                <div class="modal-body col-md-12">
+                    <div class="row form-group col-md-12">
+                        <div class="col col-md-3"><label for="text-input" class=" form-control-label">User Role</label></div>
+                        <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="e.g Administrator" class="form-control user_role_name"></div>
+                    </div>
+                    <div class="row form-group col-md-12">
+                        <div class="col col-md-3"><label for="text-input" class=" form-control-label">Description</label></div>
+                        <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="e.g Administrator CMRD" class="form-control user_role_desc"></div>
+                    </div>
+                </div>                            
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-primary">Confirm</button>
+                    <button type="button" class="btn btn-primary btn-confirm">Confirm</button>
                 </div>
             </div>
         </div>
     </div>
-    <!-- new User roles modal -->
+    <!-- new User account modal -->
 
+    <!-- deactivate modal -->
     <div class="modal fade" id="deactivateModal" tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="deactivate">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="mediumModalLabel">Deactivate User Type</h5>
+                    <h5 class="modal-title" id="mediumModalLabel">Deactivate User Account</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
                     <p>
-                        Are you sure you want to deactive this user type? 
+                        Are you sure you want to deactive this user account? 
                     </p>
                 </div>
                 <div class="modal-footer">
@@ -80,7 +87,31 @@
             </div>
         </div>
     </div>
-    <!-- modal -->
+    <!-- deactivate modal -->
+
+    <!-- accept modal -->
+    <div class="modal fade" id="acceptModal" tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="deactivate">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="mediumModalLabel">Accept User Account</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>
+                        Are you sure you want to accept this user account? 
+                    </p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-primary btn_accept_confirm">Confirm</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- accept modal -->
 
     <div id="right-panel" class="right-panel">
 
@@ -92,7 +123,7 @@
             <div class="col-sm-4">
                 <div class="page-header float-left">
                     <div class="page-title">
-                        <h1>User Type</h1>
+                        <h1>User Roles</h1>
                     </div>
                 </div>
             </div>
@@ -102,7 +133,7 @@
                         <ol class="breadcrumb text-right">
                             <li><a href="dashboard">Dashboard</a></li>
                             <li><a href="dashboard">My Organization</a></li>
-                            <li class="active">User Type</li>
+                            <li class="active">User Roles</li>
                         </ol>
                     </div>
                 </div>
@@ -116,35 +147,19 @@
                     <div class="col-md-12">
                         <div class="card">
                             <div class="card-header">
-                                <strong class="card-title">User Type List</strong>
-                                <!-- <button class="btn btn-sm btn-success float-right" type="submit" data-toggle="modal" data-target="#newUserModal"><i class="fa fa-plus-circle"></i> Add Type Roles</button> -->
+                                <strong class="card-title">User Roles List</strong>
+                                <button class="btn btn-sm btn-success float-right" type="submit" data-toggle="modal" data-target="#newDeptModal"><i class="fa fa-plus-circle"></i> Add Role</button>
                             </div>
                             <div class="card-body">
-                                <table id="bootstrap-data-table-export" class="table table-striped table-bordered">
+                                <table id="bootstrap-data-table-export" class="table table-striped table-bordered table_head">
                                     <thead>
                                         <tr>
-                                            <th>Type</th>
-                                            <th>Status</th>
-                                            <th>Actions</th>
+                                            <th>#</th>
+                                            <th>Name</th>
+                                            <th>Description</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>Administrator</td>
-                                            <td>Active</td>
-                                            <td>
-                                                <button class="btn btn-sm btn-primary" type="submit" data-toggle="modal" data-target="#updateUserModal"><i class="fa fa-refresh"></i> Update</button>
-                                                <!-- <button class="btn btn-sm btn-secondary" type="submit" data-toggle="modal" data-target="#deactivateModal"><i class="fa fa-ban"></i> Deactivate</button> -->
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>User</td>
-                                            <td>Active</td>
-                                            <td>
-                                                <button class="btn btn-sm btn-primary" type="submit" data-toggle="modal" data-target="#updateUserModal"><i class="fa fa-refresh"></i> Update</button>
-                                                <!-- <button class="btn btn-sm btn-secondary" type="submit" data-toggle="modal" data-target="#deactivateModal"><i class="fa fa-ban"></i> Deactivate</button> -->
-                                            </td>
-                                        </tr>
+                                    <tbody class="table_body">
                                     </tbody>
                                 </table>
                             </div>
@@ -163,6 +178,193 @@
 
     <?php $this->load->view('templates/footer.php'); ?> 
 
+    <script type="text/javascript">
+    ( function ( $ ) {
+
+        var ___ctx = '';
+
+        var __setContext = function(newctx) {
+            ___ctx = newctx;
+        };
+
+        var __getContext = function() {
+            return ___ctx;
+        };
+        var __executeExternalPost = function(path, jsonObj, customLoader) {
+            path = __getContext() + path;
+            var d = $.Deferred();
+            if(customLoader != ""){
+                $("#"+customLoader).show();
+                $("#"+customLoader).removeClass("hide");
+            }
+            $.ajax({
+                method: "POST",
+                url: path,
+                dataType: "json",
+                headers: {
+                    // 'Content-Type': 'multipart/form-data;'
+                    'Content-Type':'application/json'
+                },
+                data: jsonObj
+            }).done(function (data, textStatus, jqXHR) {
+                if(customLoader != ""){
+                    $("#"+customLoader).hide();
+                    $("#"+customLoader).addClass("hide");
+                }
+                d.resolve(data)
+            }).fail(function (jqXHR, textStatus, errorThrown,request) {
+                console.log('---FAILED---');
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+                console.log('---FAILED---');
+                
+                d.resolve({
+                    status : 'ERROR',
+                    message : request
+                });
+                
+                if(customLoader != ""){
+                    $("#"+customLoader).hide();
+                    $("#"+customLoader).addClass("hide");
+                }
+            });
+            
+            return d.promise();
+        };
+        var __executeExternalGet = function(path, customLoader) {
+            // path = $.wms.getContextPath() + path;
+            var d = $.Deferred();
+            if(customLoader != ""){
+                $("#"+customLoader).show();
+                $("#"+customLoader).removeClass("hide");
+            }
+            $.ajax({
+                method: "GET",
+                url: path,
+                dataType: "json",
+            }).done(function (data, textStatus, jqXHR) {
+                if(customLoader != ""){
+                    $("#"+customLoader).hide();
+                    $("#"+customLoader).addClass("hide");
+                }
+                d.resolve(data)
+            }).fail(function (jqXHR, textStatus, errorThrown,request) {
+                console.log('---FAILED---');
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+                console.log('---FAILED---');
+                
+                d.resolve({
+                    status : 'ERROR',
+                    message : request
+                });
+                
+                if(customLoader != ""){
+                    $("#"+customLoader).hide();
+                    $("#"+customLoader).addClass("hide");
+                }
+            });
+            
+            return d.promise();
+        };
+
+        $(".btn-confirm").unbind("click").on("click", function(){
+            console.log('clicked')
+
+            var payload = {
+                "name"             : $(".user_role_name").val(),
+                "description"      : $(".user_role_desc").val(),
+                "parentId"         : "2",
+                "departmentId"     : "2"
+             }
+             console.log(payload);
+
+            __executeExternalPost('http://localhost:8088/role/create',JSON.stringify(payload)).done(function (result) {
+                console.log(result);
+                if (result.status != "ERROR") {
+                    $(".form-control").val('');
+                    $('#newDeptModal').modal('hide');
+                    __table();
+                }else{
+                    console.log("failed adding new department")
+                }
+            })
+        })
+
+        var __table = function(){
+            $('.table_head').DataTable().destroy();
+            $('.table_body').empty();
+
+            __executeExternalGet('http://localhost:8088/role/list').done(function (result) {
+                console.log(result)
+                if (result.status != "ERROR") {
+                    result.forEach(function(data){
+                        console.log(data)
+                        $('.table_body').append("<tr>"+
+                            "<td>"+data.id+"</td>"+
+                            "<td>"+data.name+"</td>"+  
+                            "<td>"+data.description+"</td>");
+                    });
+                } else {
+                    console.log("failed fetching department list")
+                }
+                
+                $(document).ready(function () {
+                    var table = $('.table_head').DataTable({
+                        order: [[0, 'asc']],
+                        // "columnDefs": [
+                            // { "width": "30%", "targets": 6 }
+                        // ]
+                    });
+                    $('.dataTables_length').addClass('bs-select');
+                });
+
+                // $(".btn_update").unbind("click").on("click", function(){
+                //     console.log("clicked button update")
+                //     var data_id = $(this).data("id");
+                //     console.log(data_id)
+                //     __executeExternalGet('http://localhost:8088/department/'+data_id).done(function (result) {
+                //         console.log(result);
+                //         if (result.status != "ERROR") {
+                //             $(".dep_name_update").val(result.name);
+                //             $(".dep_desc_update").val(result.description);
+
+                //             $(".btn_confirm_update").unbind("click").on("click", function(){
+                //                 console.log('clicked btn update confirm')
+                //                 var payload = {
+                //                     "updatedBy"     : "1",
+                //                     "name"          : $(".dep_name_update").val(),
+                //                     "description"   : $(".dep_desc_update").val(),
+                //                     "parentId"      : "1",
+                //                     "locationId"    : "1"
+                //                 }
+                //                 console.log(payload);
+                //                 __executeExternalPost('http://localhost:8088/department/update/'+data_id,JSON.stringify(payload)).done(function (result) {
+                //                     console.log(result);
+                //                     if (result.status != "ERROR") {
+                //                         $(".form-control").val('');
+                //                         $('#updateDeptModal').modal('hide');
+                //                         __table();
+                //                     }else{
+                //                         alert("failed")
+                //                     }
+                //                 })
+                //             })
+
+                //         }else{
+                //             alert("failed")
+                //         }
+                //     })
+                // })
+            })
+        }
+        __table();
+
+
+    } )( jQuery );
+    </script>
 
 </body>
 
