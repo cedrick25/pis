60c5adaaf0af68acd0c0996a81d589620ad3237f
derycedrick <derycedrick@gmail.com>
2023-01-09 01:13:08 -0800

    minor changes

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -154,7 +154,7 @@
                 window.location.href="./"
             },1000);
         })
-                
+
         $('.form-control').keyup(function(event) {
             var textBox = event.target;
             var start = textBox.selectionStart;
@@ -162,6 +162,11 @@
             textBox.value = textBox.value.charAt(0).toUpperCase() + textBox.value.slice(1);
             textBox.setSelectionRange(start, end);                  
         });
+        
+        // $('.dropdown hh').on('click', function() {
+        // $('.dropdown hh').removeClass('active');
+        // $(this).addClass('active');
+        // });
 
     } )( jQuery );
     </script>
