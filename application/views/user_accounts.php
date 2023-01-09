21153e5dadc6e4eec0f1f24bae9989c5147af314
hansel <mejarito.hansel@gmail.com>
2023-01-09 13:32:16 +0800

    user account module modified, birthday removed in display list

--- application/views/user_accounts.php
+++ application/views/user_accounts.php
@@ -508,7 +508,7 @@
                     $(document).ready(function () {
                         $('.table_head tbody tr').each(function (idx) {
                            $(this).children("td:eq(0)").html(idx + 1);
-                       });
+                        });
                         var table = $('.table_head').DataTable({
                             order: [[0, 'asc']],
                             "columnDefs": [
