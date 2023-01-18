90314500fa0e7fb8cf17dd812faa39a4a1980b4b
derycedrick <derycedrick@gmail.com>
2023-01-19 02:10:26 +0800

    supervision forward page UI supervision create new docket UI suervision update UI all page is ready for integration

--- application/controllers/Routes.php
+++ application/controllers/Routes.php
@@ -134,9 +134,17 @@ class routes extends CI_Controller {
 	{
 		$this->load->view('supervision_docket_create');
 	}
+	public function supervision_docket_update()
+	{
+		$this->load->view('supervision_docket_update');
+	}
 	public function inv_forward()
 	{
 		$this->load->view('inv_forward');
+	}
+	public function sup_forward()
+	{
+		$this->load->view('sup_forward');
 	}
 
 }
