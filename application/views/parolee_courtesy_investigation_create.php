0b80ecf10ba7a1b103ae24b310861cb931715656
hansel <mejarito.hansel@gmail.com>
2023-02-03 01:52:17 +0800

    parole ci create

--- application/views/parolee_courtesy_investigation_create.php
+++ application/views/parolee_courtesy_investigation_create.php
@@ -249,24 +249,6 @@
             $(".form-control").val('');
         });
 
-        var __select = function(){
-            $('.field_office').empty();
-
-            __executeExternalGet('http://localhost:8088/department/list').done(function (result) {
-                // console.log(result)
-                if (result.status != "ERROR") {
-                    $('.field_office').append("<option selected disabled> - - Select Field Office - - </option>");
-                    result.forEach(function(data){
-                        $('.field_office').append(
-                            "<option value="+data.id+">"+data.name+"</option>");
-                    });
-                   
-                } else {
-                    console.log("failed fetching docket list")
-                }
-            })
-        }
-        __select();
         $(".btn-confirm").unbind("click").on("click", function(){
             
             var payload = {
