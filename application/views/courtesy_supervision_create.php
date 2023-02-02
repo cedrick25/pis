764bd51f6cdd9da5d3002fcf0ccd0942ea5bd122
derycedrick <derycedrick@gmail.com>
2023-02-02 23:14:11 +0800

    add new courtesy investigation form

--- application/views/courtesy_supervision_create.php
+++ application/views/courtesy_supervision_create.php
@@ -44,60 +44,93 @@
 			                        <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="e.g John" class="form-control docket_num"></div>
 			                    </div>
 			                    <div class="row form-group col-md-6">
-			                        <div class="col col-md-3"><label for="text-input" class=" form-control-label">Referring Office</label></div>
-			                        <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="e.g A." class="form-control ref_office"></div>
-			                    </div>
-			                    <div class="row form-group col-md-6">
 			                        <div class="col col-md-3"><label for="text-input" class=" form-control-label">Supervising Officer</label></div>
 			                        <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="e.g Doe" class="form-control sup_officer"></div>
 			                    </div>
-                                <div class="row form-group col-md-12">
-                                        <div class="col col-md-1"><label for="text-input" class=" form-control-label">Reasons for referral</label></div>
-                                        <div class="col-12 col-md-11"><textarea rows="2" cols="50" class="form-control reasons"></textarea></div>
+                                <div class="row form-group col-md-6">
+                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Referring Office</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="e.g A." class="form-control ref_office"></div>
                                 </div>
                                 <div class="row form-group col-md-6">
-                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Sex</label></div>
+                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Reason for referral</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="e.g Parolee" class="form-control reason"></div>
+                                </div>
+                                <div class="row form-group col-md-6">
+                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Task</label></div>
                                     <div class="col-12 col-md-9">
                                         <select class="form-control caseload select2">
                                             <option selected value="none" disabled>Select</option>
-                                            <option value="male">Male</option>
-                                            <option value="female">Female</option>
+                                            <option value="SINGLE_CARPETA_PAPWA">Process Application for Permit to Work Abroad</option>
+                                            <option value="SINGLE_CARPETA_PAR">Process Arrival Report</option>
+                                            <option value="SINGLE_CARPETA_PBR">Process Briefing Report</option>
+                                            <option value="SINGLE_CARPETA_PCV">Process Case Verification</option>
+                                            <option value="SINGLE_CARPETA_PCU">Process Certificate of Undertaking</option>
+                                            <option value="SINGLE_CARPETA_PCS">Process Courtesy Supervision</option>
+                                            <option value="SINGLE_CARPETA_PDR">Process Death Report</option>
+                                            <option value="SINGLE_CARPETA_PGIOR">Process GIOR</option>
+                                            <option value="SINGLE_CARPETA_PIR">Process Infraction Report</option>
+                                            <option value="SINGLE_CARPETA_PORBPP">Process Other Requests by BPP</option>
+                                            <option value="SINGLE_CARPETA_PPP">Process Program of Payment</option>
+                                            <option value="SINGLE_CARPETA_PPR">Process Progress Report</option>
+                                            <option value="SINGLE_CARPETA_PRC">Process Records Check</option>
+                                            <option value="SINGLE_CARPETA_PRCPC">Process Request for Certificate of No Pending Case</option>
+                                            <option value="SINGLE_CARPETA_PRCNA">Process Request for Certificate of Non-Appeal</option>
+                                            <option value="SINGLE_CARPETA_PRCO">Process Request for Commitment Order</option>
+                                            <option value="SINGLE_CARPETA_PRCI">Process Request for Community Interview</option>
+                                            <option value="SINGLE_CARPETA_PRCD">Process Request for Court's Decision</option>
+                                            <option value="SINGLE_CARPETA_PRDGC">Process Request for Decision Guide Chart</option>
+                                            <option value="SINGLE_CARPETA_PREJ">Process Request for Entry of Judgment</option>
+                                            <option value="SINGLE_CARPETA_PRFI">Process Request for Fiscal's Information</option>
+                                            <option value="SINGLE_CARPETA_PRPD">Process Request for Permanent Dismissal</option>
+                                            <option value="SINGLE_CARPETA_PRPDLP">Process Request for Pertinent documents of Local Prisoners</option>
+                                            <option value="SINGLE_CARPETA_PRPSIR">Process Request for Post Sentence IR (Absolute Pardon)</option>
+                                            <option value="SINGLE_CARPETA_PRPECIR">Process Request for Pre-EC Investigation Report</option>
+                                            <option value="SINGLE_CARPETA_PRPPIR">Process Request for Pre-parole Investigation Report</option>
+                                            <option value="SINGLE_CARPETA_PRTR">Process Request for Transfer of Residence(FO)</option>
+                                            <option value="SINGLE_CARPETA_PRTR">Process Request for Transfer of Residence(TSD)</option>
+                                            <option value="SINGLE_CARPETA_PSTR">Process Status Report</option>
+                                            <option value="SINGLE_CARPETA_PSR">Process Summary Report</option>
+                                            <option value="SINGLE_CARPETA_PVT">Process Verify Threats</option>
+                                            <option value="SINGLE_CARPETA_PVR">Process Violation Report</option>
+                                            <option value="SINGLE_CARPETA_PWR">Process Where to Reside</option>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="row form-group col-md-6">
-                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Education</label></div>
-                                    <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="e.g College" class="form-control education"></div>
-                                </div>
-                                <div class="row form-group col-md-6">
-                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Occupation</label></div>
-                                    <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="e.g Officer" class="form-control occupation"></div>
-                                </div>
-                                <div class="row form-group col-md-6">
-                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Criminal Case No.</label></div>
-                                    <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="e.g No.1234" class="form-control cc_no."></div>
-                                </div>
-                                <div class="row form-group col-md-6">
-                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Field Office</label></div>
+                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Client Type</label></div>
                                     <div class="col-12 col-md-9">
-                                        <select class="form-control field_office select2">
-                                            <!-- <option selected value="ADULT">Adult</option>
-                                            <option value="JUVENILE">Juvenile</option> -->
+                                        <select class="form-control client_type select2">
+                                            <option selected value="select">Select</option>
+                                            <!-- <option value="JUVENILE">Juvenile</option> -->
                                         </select>
                                     </div>
                                 </div>
-			                    <div class="row form-group col-md-6">
-			                        <div class="col col-md-3"><label for="text-input" class=" form-control-label">Birthdate</label></div>
-			                        <div class="col-12 col-md-9"><input type="date" class="form-control birthdate"></div>
-			                    </div>
-			                    <div class="row form-group col-md-6">
-			                        <div class="col col-md-3"><label for="text-input" class=" form-control-label">Birth Place</label></div>
-			                        <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="e.g Quezon" class="form-control b_place"></div>
-			                    </div>
                                 <div class="row form-group col-md-6">
-                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Address</label></div>
-                                    <div class="col-12 col-md-9"><input type="text" name="text-input" placeholder="e.g Marikina" class="form-control b_place"></div>
+                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Case Class</label></div>
+                                    <div class="col-12 col-md-9">
+                                        <select class="form-control case_class select2">
+                                            <option selected value="select">Select</option>
+                                            <!-- <option value="JUVENILE">Juvenile</option> -->
+                                        </select>
+                                    </div>
                                 </div>
+                                <div class="row form-group col-md-6">
+                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Date Received by PPO</label></div>
+                                    <div class="col-12 col-md-9"><input type="date" class="form-control dr_ppo"></div>
+                                </div>
+                                <div class="row form-group col-md-6">
+                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Start Supervision</label></div>
+                                    <div class="col-12 col-md-9"><input type="date" class="form-control start_sup_date"></div>
+                                </div>
+                                <div class="row form-group col-md-6">
+                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">End Supervision</label></div>
+                                    <div class="col-12 col-md-9"><input type="date" class="form-control end_sup_date"></div>
+                                </div>
+                                
+			                    <div class="row form-group col-md-6">
+			                        <div class="col col-md-3"><label for="text-input" class=" form-control-label">Date Courtesy Referral</label></div>
+			                        <div class="col-12 col-md-9"><input type="date" class="form-control date_court_ref"></div>
+			                    </div>
                             </div>
                             <div class="card-footer">
 			                    <button type="button" class="btn btn-secondary btn-sm btn-reset">Reset</button>
