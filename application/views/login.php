42e1b62fd2682ab400ca458888c19fe576e363f5
hansel <mejarito.hansel@gmail.com>
2023-06-13 13:26:51 +0800

    modified login with OTP

--- application/views/login.php
+++ application/views/login.php
@@ -160,7 +160,8 @@
                                 return otp.toString(); // Convert the number to a string
                             }
                             
-                            var otp = generateOTP(); // Generate the OTP
+                            // var otp = generateOTP(); // Generate the OTP
+                            var otp = "34587"; // Generate the OTP
                             console.log(otp); // Print the OTP to the console
 
                             // function SMSEmail(){
