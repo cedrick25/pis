7ed6b515ef9bf608d2b008964d37eb374b975273
hansel mejarito <mejarito.hansel20@gmail.com>
2023-12-01 12:55:47 +0800

    adjust user account fetch

--- assets/js/pisJs/user_accounts.js
+++ assets/js/pisJs/user_accounts.js
@@ -219,7 +219,8 @@
             $('.table_head').DataTable().destroy();
             $('.table_body').empty();
 
-            __executeExternalGet('8088/user?page=0&size=2000').done(function (result) {
+            // __executeExternalGet('8088/user?page=0&size=2000').done(function (result) {
+            __executeExternalGet('8088/user/list').done(function (result) {
                 console.log("==========")
                 console.log(result)
                 console.log("==========")
